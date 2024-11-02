0a2e2a7bab4ddd023705c9cb383e058eedbfde0a
Fabio Accurso <oibaf15@gmail.com>
2024-11-02 21:31:55 +0100

    aggiunta esercizi completi backtraking

---
+++ backtracking/combinationSum/combinationSum.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool is_a_solution(int sum, int target){
+    if(sum==target)
+        return true;
+    else
+        return false;
+}
+
+bool is_safe(int sum, int candidato, int target){
+    if(sum+candidato<=target)
+        return true;
+    else
+        return false;
+}
+
+void make_move(vector<int>& subresult, int candidato, vector<vector<int>>& risultato){
+   subresult.push_back(candidato);
+}
+
+
+void unmake_move(vector<int>& subresult){
+    subresult.pop_back();
+}
+
+
+void backtrack(vector<int>& candidates, vector<vector<int>>& risultato, int target, int sum, vector<int>& subresult, int attuale){
+    if(is_a_solution(sum,target)){
+        risultato.push_back(subresult);
+        return;
+    }
+    else{
+        for(int i=0;i<candidates.size();i++){
+            if(is_safe(sum,candidates[i],target) && candidates[i]>=attuale){
+                sum=sum+candidates[i];  //makemove
+                make_move(subresult,candidates[i],risultato);
+                backtrack(candidates,risultato,target,sum,subresult,candidates[i]);
+                sum=sum-candidates[i];  //unmake_move();
+                unmake_move(subresult);
+            }
+        }
+    }
+
+}
+
+vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> risultato;
+    int sum=0,out=0,attuale=0;
+    vector<int> subresult;
+    backtrack(candidates,risultato,target,sum,subresult,attuale);
+
+    return risultato;
+
+}
+
+int main(){
+    vector<int> input = {2,3,5};
+    int target = 8;
+
+    vector<vector<int>> soluzione= combinationSum(input,target);
+    //output atteso [[2,2,2,2],[2,3,3],[3,5]]
+    if(soluzione.size()==0){
+        cout<<"Non ci sono soluzioni";
+    }
+    else{
+        for(int i=0;i<soluzione.size();i++){
+            cout<<i+1<<"a combinazione possibile -> ";
+            for(int j=0;j<soluzione[i].size();j++){
+                cout<<soluzione[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }   
+}
---
+++ backtracking/tastierinoNumerico/tastierinoNumerico.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve(string digit, string output, int i, vector<string>& ans, string mapping[]) {
+    if (i >= digit.length()) {
+        ans.push_back(output);
+        return;
+    }
+
+    int num = digit[i] - '0';
+    string value = mapping[num];
+
+    for (int j = 0; j < value.length(); j++) {
+        output.push_back(value[j]);
+        solve(digit, output, i + 1, ans, mapping);
+        output.pop_back();
+    }
+}
+
+
+
+vector<string> letterCombinations(string digit) {
+    vector<string> ans;
+    if (digit.length() == 0) {
+        return ans;
+    }
+    int i = 0;
+    string output;
+    string mapping[10] = {"",    "",    "abc",  "def", "ghi",
+                            "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    solve(digit, output, i, ans, mapping);
+    return ans;
+}
+
+
+
+int main() {
+    string digits = "23";
+    vector<string> result = letterCombinations(digits);
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
