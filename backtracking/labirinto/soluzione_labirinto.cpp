5a6c6faf9afcf3d5c684603c89304890598a8bea
Fabio Accurso <oibaf15@gmail.com>
2024-11-02 12:25:51 +0100

    Completato esercizio labirinto

--- backtracking/labirinto/soluzione_labirinto.cpp
+++ backtracking/labirinto/soluzione_labirinto.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 
 int N=4;
-int matrice[4][4]={1,0,0,0,1,1,0,1,0,1,0,0,1,1,1,1};
-int soluzione[4][4]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int matrice[4][4]={1,1,0,0,1,1,1,0,1,0,1,0,1,0,1,1};
+int soluzione[4][4]={1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
 
 bool backtrack(int riga, int colonna){
-    if(riga==N-1 && colonna==N-1){
+    if(riga==N-1 && colonna==N-1 && matrice[riga][colonna]==1){
         //aggiungere 1 a soluzione
         soluzione[riga][colonna]=1;
         return true;
     }
     if(riga<N&&riga>=0&&colonna<N&&colonna>=0){
-        if(backtrack(riga+1,colonna)==1){
+        if(matrice[riga+1][colonna]==1 && backtrack(riga+1,colonna)==true){
             soluzione[riga+1][colonna]=1;
             return true;
         }
-        if(backtrack(riga,colonna+1)==1){
+        if(matrice[riga][colonna+1]==1 && backtrack(riga,colonna+1)==true){
             soluzione[riga][colonna+1]=1;
             return true;
         }
@@ -40,6 +40,7 @@ int main(){
         cout<<endl;
     }
 
+    cout<<"\nMatrice soluzione:"<<endl;
     if(backtrack(0,0)){
         //stampa
         for(int i = 0;i<N;i++){
@@ -49,6 +50,4 @@ int main(){
             cout<<endl;
         }
     }
-
-
 }
